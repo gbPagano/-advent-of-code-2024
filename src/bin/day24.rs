6ec0ec0d66c3a1d9e858fcb2a582a991b2ac7771
gbPagano <guilhermebpagano@gmail.com>
2024-12-29 01:23:06 -0300

    feat: add day 24 part 2

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -3,7 +3,7 @@ use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::Read;
 
-// difficulty: ?
+// difficulty: 5
 
 fn main() {
     //let mut file = File::open("inputs/day24.test").unwrap();
@@ -12,10 +12,10 @@ fn main() {
     file.read_to_string(&mut data).unwrap();
 
     part_one(data.clone());
-    //part_two(data);
+    part_two(data);
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Operation {
     AND,
     OR,
@@ -90,4 +90,89 @@ fn part_one(data: String) {
         .fold(0, |acc, (idx, x)| acc | x.1 << idx);
 
     println!("Result part 1: {result}");
+}
+
+fn is_xy(conn: &Connection) -> bool {
+    conn.x.starts_with("x") && conn.y.starts_with("y")
+        || conn.x.starts_with("y") && conn.y.starts_with("x")
+}
+
+fn conn_is_valid(conn: &Connection, connections: &VecDeque<Connection>, max_z: &str) -> bool {
+    if conn.z.starts_with("z") && conn.op != Operation::XOR && conn.z != max_z {
+        return false;
+    } else if conn.z == max_z && conn.op != Operation::OR {
+        return false;
+    } else if is_xy(&conn) && conn.z != "z00" && !(conn.x.ends_with("00") && conn.y.ends_with("00"))
+    {
+        if conn.op == Operation::XOR
+            && connections
+                .iter()
+                .filter(|c| {
+                    [c.x, c.y].contains(&conn.z) && [Operation::XOR, Operation::AND].contains(&c.op)
+                })
+                .count()
+                != 2
+        {
+            return false;
+        } else if conn.op == Operation::AND
+            && connections
+                .iter()
+                .filter(|c| [c.x, c.y].contains(&conn.z) && c.op == Operation::OR)
+                .count()
+                == 0
+        {
+            return false;
+        } else if conn.op == Operation::OR {
+            return false;
+        }
+    } else if !conn.z.starts_with("z") && !is_xy(&conn) && conn.op == Operation::XOR {
+        return false;
+    }
+    true
+}
+
+fn part_two(data: String) {
+    let wires_re = Regex::new(r"(.+): (\d)").unwrap();
+    let conn_re = Regex::new(r"(.+) (.+) (.+) .+ (.+)").unwrap();
+    let (start_wires, raw_connections) = data.split_once("\n\n").unwrap();
+
+    let mut wires: HashMap<&str, usize> = HashMap::new();
+    for capture in wires_re.captures_iter(start_wires) {
+        let key = capture.get(1).unwrap().as_str();
+        let val = capture.get(2).unwrap().as_str();
+        wires.insert(key, val.parse().unwrap());
+    }
+
+    let mut connections = VecDeque::new();
+    for capture in conn_re.captures_iter(raw_connections) {
+        let x = capture.get(1).unwrap().as_str();
+        let op = Operation::from_str(capture.get(2).unwrap().as_str());
+        let y = capture.get(3).unwrap().as_str();
+        let z = capture.get(4).unwrap().as_str();
+
+        connections.push_back(Connection { x, op, y, z });
+    }
+
+    let max_z = connections
+        .iter()
+        .filter_map(|item| {
+            if item.z.starts_with("z") {
+                Some(item.z)
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap();
+
+    let mut wrongs = Vec::new();
+    for conn in &connections {
+        if !conn_is_valid(conn, &connections, &max_z) {
+            wrongs.push(conn.z);
+        }
+    }
+    wrongs.sort();
+    
+    let result = wrongs.join(",");
+    println!("Result part 2: {result}");
 }
